b1a286141ae24de8bca564a5a6b7624708ed4bf1
Gabriela <gzmkowalik@gmail.com>
2018-10-15 18:45:30 +0200

    UX improvements

--- app/src/main/java/pl/edu/agh/sarna/report/ReportActivity.kt
+++ app/src/main/java/pl/edu/agh/sarna/report/ReportActivity.kt
@@ -22,6 +22,62 @@ import java.lang.StringBuilder
 
 
 class ReportActivity : AppCompatActivity(), AsyncResponse {
+    private var rootState: Boolean = false
+    private var eduState: Boolean = false
+    private var serverState: Boolean = false
+    private var reportState: Boolean = false
+    private var processID: Long = 0
+    private val successDrawable = GradientDrawable()
+    private val failDrawable = GradientDrawable()
+
+    private val layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_report)
+        initialiseLayout()
+        initialiseOptions()
+        DbReportTask(this, this, processID).execute()
+
+    }
+
+    private fun extendedReportOnClickListener(runID: Long): View.OnClickListener? {
+        return View.OnClickListener {view ->
+            when(view.tag) {
+                getString(R.string.wifi_title) -> generateExtendedWifiReport(runID)
+            }
+
+        }
+    }
+
+    private fun generateExtendedWifiReport(runID: Long) {
+        WifiPasswordsReportTask(this, this, runID).execute()
+    }
+
+    private fun initialiseLayout() {
+        layoutParams.setMargins(0, 0, 0, 10)
+
+        successDrawable.setColor(Color.rgb(0, 204, 102))
+        successDrawable.cornerRadius = 10f
+        successDrawable.setStroke(1, Color.WHITE)
+
+        failDrawable.setColor(Color.rgb(255, 51, 51))
+        failDrawable.cornerRadius = 10f
+        failDrawable.setStroke(1, Color.WHITE)
+    }
+
+    override fun onBackPressed() {
+
+    }
+    private fun initialiseOptions() {
+        rootState = intent.getBooleanExtra("root_state", false)
+        eduState = intent.getBooleanExtra("edu_state", false)
+        serverState = intent.getBooleanExtra("server_state", false)
+        reportState = intent.getBooleanExtra("report_state", false)
+        processID = intent.getLongExtra("process_id", 0)
+    }
+
     override fun processFinish(output: Any) {
         val builder =  AlertDialog.Builder(this)
         val info = StringBuilder()
@@ -31,7 +87,7 @@ class ReportActivity : AppCompatActivity(), AsyncResponse {
 
         builder.setTitle("Details")
                 .setMessage(
-                    info
+                        info
                 )
                 .setPositiveButton("OK") { _, _ -> }
                 .show();
@@ -52,78 +108,10 @@ class ReportActivity : AppCompatActivity(), AsyncResponse {
             reportLayout.addView(button, layoutParams)
         }
 
-        insertFinishButton()
     }
-
-    private var rootState: Boolean = false
-    private var eduState: Boolean = false
-    private var serverState: Boolean = false
-    private var reportState: Boolean = false
-    private var processID: Long = 0
-    private val successDrawable = GradientDrawable()
-    private val failDrawable = GradientDrawable()
-
-    private val layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-    private val wrappedParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-
     @RequiresApi(Build.VERSION_CODES.JELLY_BEAN)
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_report)
-        initialiseLayout()
-        initialiseOptions()
-        DbReportTask(this, this, processID).execute()
-
-    }
-
-    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN)
-    private fun insertFinishButton() {
-        val button = Button(this)
-        button.tag = R.string.wifi_title
-        button.text = "Exit"
-        button.setOnClickListener { _ ->
-            this.finishAffinity()
-            System.exit(0)
-        }
-
-        reportLayout.addView(button, wrappedParams)
-    }
-
-
-    private fun extendedReportOnClickListener(runID: Long): View.OnClickListener? {
-        return View.OnClickListener {view ->
-            when(view.tag) {
-                getString(R.string.wifi_title) -> generateExtendedWifiReport(runID)
-            }
-
-        }
-    }
-
-    private fun generateExtendedWifiReport(runID: Long) {
-        WifiPasswordsReportTask(this, this, runID).execute()
-    }
-
-    private fun initialiseLayout() {
-        layoutParams.setMargins(0, 0, 0, 10)
-        wrappedParams.setMargins(125, 20, 125, 0)
-
-        successDrawable.setColor(Color.rgb(0, 204, 102))
-        successDrawable.cornerRadius = 10f
-        successDrawable.setStroke(1, Color.WHITE)
-
-        failDrawable.setColor(Color.rgb(255, 51, 51))
-        failDrawable.cornerRadius = 10f
-        failDrawable.setStroke(1, Color.WHITE)
-    }
-
-    override fun onBackPressed() {
-
-    }
-    private fun initialiseOptions() {
-        rootState = intent.getBooleanExtra("root_state", false)
-        eduState = intent.getBooleanExtra("edu_state", false)
-        serverState = intent.getBooleanExtra("server_state", false)
-        reportState = intent.getBooleanExtra("report_state", false)
-        processID = intent.getLongExtra("process_id", 0)
+    fun terminateApp(view : View){
+        this.finishAffinity()
+        System.exit(0)
     }
 }
