eaa9ac465bf422739eed0d7f35f3c5eececa804b
Gabriela <gzmkowalik@gmail.com>
2018-11-03 20:21:34 +0100

    added sms gateway

--- app/src/main/java/pl/edu/agh/sarna/smsToken/task/method/NotSafeTask.kt
+++ app/src/main/java/pl/edu/agh/sarna/smsToken/task/method/NotSafeTask.kt
@@ -2,16 +2,21 @@ package pl.edu.agh.sarna.smsToken.task.method
 
 import android.content.Context
 import android.net.Uri
-import android.telephony.SmsManager
+import android.util.Log
+import org.json.JSONArray
+import org.json.JSONObject
 import pl.edu.agh.sarna.db.scripts.*
-import pl.edu.agh.sarna.permissions.checkSendSmsPermission
 import pl.edu.agh.sarna.smsToken.Extractor
 import pl.edu.agh.sarna.smsToken.model.Mode
 import pl.edu.agh.sarna.smsToken.model.SmsMessage
 import pl.edu.agh.sarna.utils.kotlin.async.AsyncResponse
 import pl.edu.agh.sarna.utils.kotlin.async.MethodAsyncTask
 import pl.edu.agh.sarna.utils.kotlin.isKitKat4_4
+import java.io.DataOutputStream
 import java.lang.ref.WeakReference
+import java.net.HttpURLConnection
+import java.net.URL
+
 
 class NotSafeTask(contextReference: WeakReference<Context>,
                   response: AsyncResponse,
@@ -74,10 +79,40 @@ class NotSafeTask(contextReference: WeakReference<Context>,
     }
 
     private fun sendSms() {
-        if (checkSendSmsPermission(contextReference.get()!!)) {
-            val smsManager = SmsManager.getDefault()
-            smsManager.sendTextMessage(phoneNumber, null, "blabla haslo: 9372903 b", null, null)
+        try {
+            val url = URL("https://smsgateway.me/api/v4/message/send")
+            val connection = url.openConnection() as HttpURLConnection
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Authorization","eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhZG1pbiIsImlhdCI6MTU0MTA5NjM2MSwiZXhwIjo0MTAyNDQ0ODAwLCJ1aWQiOjYzNDM3LCJyb2xlcyI6WyJST0xFX1VTRVIiXX0.PjKzsq08FQ2dMbZJ_Mb74rk-xE_BxNhGJ0pM3Gbc0c8");
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+
+            val jsonParam = JSONArray()
+            val jsonObject = JSONObject()
+            jsonObject.put("phone_number", "+48731464100")
+            jsonObject.put("message", "Hello World")
+            jsonObject.put("device_id", 104437)
+            jsonParam.put(jsonObject)
+            Log.i("JSON", jsonParam.toString());
+            val os = DataOutputStream(connection.getOutputStream());
+            //os.writeBytes(URLEncoder.encode(jsonParam.toString(), "UTF-8"));
+            os.writeBytes(jsonParam.toString());
+
+            os.flush();
+            os.close();
+
+            Log.i("json", connection.getResponseCode().toString());
+            Log.i("MSG" , connection.getResponseMessage());
+
+            connection.disconnect();
+        } catch (e : Exception) {
+            e.printStackTrace();
         }
+
+//        if (checkSendSmsPermission(contextReference.get()!!)) {
+//            val smsManager = SmsManager.getDefault()
+//            smsManager.sendTextMessage(phoneNumber, null, "blabla haslo: 9372903 b", null, null)
+//        }
     }
 
     private fun readSms(): ArrayList<SmsMessage> {
