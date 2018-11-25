54d452e098c2d5ec0f15a2b5094604fe1a4ebdc9
Mateusz Olczyk <matik60@gmail.com>
2018-11-25 22:48:41 +0100

    MongoDB: Cloak&Dagger scripts

---
+++ app/src/main/java/pl/edu/agh/sarna/db/mongo/scripts/CloakScripts.java
@@ -0,0 +1,44 @@
+package pl.edu.agh.sarna.db.mongo.scripts;
+
+import android.util.Log;
+
+import org.bson.Document;
+
+import pl.edu.agh.sarna.db.model.cloak.CloakInfo;
+import pl.edu.agh.sarna.db.model.cloak.CloakText;
+import pl.edu.agh.sarna.db.mongo.MongoDb;
+import pl.edu.agh.sarna.db.mongo.MongoDbException;
+
+public class CloakScripts {
+
+    public static void saveCloakInfoToMongo(long processID, long startTime, long endTime, boolean
+            status) {
+        final String TABLE_NAME = CloakInfo.CloakInfoEntry.TABLE_NAME;
+        MongoDb mongoDb = new MongoDb();
+        Document document = new Document()
+                .append(CloakInfo.CloakInfoEntry.COLUMN_NAME_PROCESS_ID, processID)
+                .append(CloakInfo.CloakInfoEntry.COLUMN_NAME_START_TIME, startTime)
+                .append(CloakInfo.CloakInfoEntry.COLUMN_NAME_END_TIME, endTime)
+                .append(CloakInfo.CloakInfoEntry.COLUMN_NAME_STATUS, status);
+        try {
+            mongoDb.saveData(TABLE_NAME, document);
+        } catch (MongoDbException e) {
+            Log.e(MongoDb.LOG_TAG, e.getMessage());
+        }
+    }
+
+    public static void saveCloakTextToMongo(long runID, String text, String package_) {
+        final String TABLE_NAME = CloakText.CloakTextEntry.TABLE_NAME;
+        MongoDb mongoDb = new MongoDb();
+        Document document = new Document()
+                .append(CloakText.CloakTextEntry.COLUMN_NAME_RUN_ID, runID)
+                .append(CloakText.CloakTextEntry.COLUMN_NAME_TEXT, text)
+                .append(CloakText.CloakTextEntry.COLUMN_NAME_PACKAGE, package_);
+        try {
+            mongoDb.saveData(TABLE_NAME, document);
+        } catch (MongoDbException e) {
+            Log.e(MongoDb.LOG_TAG, e.getMessage());
+        }
+    }
+    
+}
